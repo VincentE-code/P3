07ae75800b8bf624453420a23ef0790192029339
Vincent Esconobiet <vesconobiet@gmail.com>
2020-08-03 23:24:22 -0700

    modifiction class maze.py

--- maze.py
+++ maze.py
@@ -1,66 +1,35 @@
-import macgyver
-import gardien
-import objects
+class Maze:
 
-class maze:
+    def __init__(self):
+        print("object created")
 
-	with open('maze.txt') as file:
-(
-MMMMMMMMMMMMMMM
-SCCCCCCCCCBCCCM
-MMMMMMMCMMMMMCM
-MCCCCCMCMCCCMCM 
-MCMCCCMCMCGCCCM
-MCMCMMMCMMMMMCM 
-MCMCMCCCCCCCMFM
-MCMCMCCCCCCCMCM
-MCMCMMMMMMMMMCM
-MCMCCECCCCCCCCM
-MCMMMMMMMMMMMCM
-MCMCCCCCCDCCCCM
-MCMMMMMMMMMMMCM
-MCCCACCCCCCCCCM
-MMMMMMMMMMMMMMM
-)
+    def set_grid(self):
+        with open('maze.txt') as maze:
+            datas = maze.read()
+            # je parcours toutes mes lettres
+            # le retour a la ligne est le caractère "\n"
+            for letter in datas:
+                if letter == "M":
+                    print("wall")
+                    x = x + 1
+                if letter == "A":
+                    print("start")
+                    x = x + 1
+                if letter == "C":
+                    print("path")
+                    x = x + 1
+                if letter == "S":
+                    print("exit")
+                    x = x + 1
+                if letter == "\n":
+                    print("Line break")
+                    x = 0
+                    y = y + 1
+    
 
-	def __init__(self, floor_image, structure_image, M, C, S, A, B, D, E, F, G):
-        self.floor_image = floor_image
-        self.wall_image = wall_image
-        self.M = wall
-        self.C = floor
-        self.S = exit
-        self.A = macgyver()
-        self.B = guardian()
-        self.D = needle()
-        self.E = syringe()
-        self.F = plastic_tube()
-        self.G = ether()
-
-floor_image = floor_image.png
-wall_image = wall_image.png        
-
-{
-    (x, y): "M",
-    (x, y): "C",
-    (x, y): "A",
-    (x, y): "B",
-    (x, y): "D",
-    (x, y): "E",
-    (x, y): "F",
-    (x, y): "G",
-}        
-
-  file_contents = file.read()
-    print(file_contents)
+if _name_ == "__main__":
+    # ici tu test ton code
+    maze = Maze()
+    maze.set_grid()
 
 
-
-if backpack_MacGyver() == 4 and macgyver() == guardian()
-    print("Bravo! Tu as gagné!")
-elif backpack_MacGyver() < 4 and macgyver() == guardian()
-    print("game over! Tu as perdu")
-    continue
-
-if macgyver() == exit and backpack_MacGyver() == 4
-    print("Bravo! Tu as gagné!")    
-    continue
