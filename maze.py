946df93b7fbdf261e8735a77b80f9c16c3b81891
Vincent Esconobiet <vesconobiet@gmail.com>
2020-08-13 23:53:59 -0700

    Avancement classe Labyrinthe

--- maze.py
+++ maze.py
@@ -2,34 +2,38 @@ class Maze:
 
     def __init__(self):
         print("object created")
+        self.grid = {}
 
     def set_grid(self):
         with open('maze.txt') as maze:
             datas = maze.read()
-            # je parcours toutes mes lettres
-            # le retour a la ligne est le caractère "\n"
-            for letter in datas:
-                if letter == "M":
-                    print("wall")
-                    x = x + 1
-                if letter == "A":
-                    print("start")
-                    x = x + 1
-                if letter == "C":
-                    print("path")
-                    x = x + 1
-                if letter == "S":
-                    print("exit")
-                    x = x + 1
-                if letter == "\n":
-                    print("Line break")
-                    x = 0
-                    y = y + 1
-    
+        # je parcours toutes mes lettres
+        # le retour a la ligne est le caractère "\n"
+        x = 0
+        y = 0
+        for letter in datas:
+            print(x, y)
+            if letter == "M":
+                self.grid[(x, y)] = "wall"
+                x = x + 1
+            if letter == "A":
+                self.grid[(x, y)] = "start"
+                x = x + 1
+            if letter == "C":
+                self.grid[(x, y)] = "path"
+                x = x + 1
+            if letter == "S":
+                self.grid[(x, y)] = "exit"
+                x = x + 1
+            if letter == "\n":
+                print("line break")
+                x = 0
+                y = y + 1
 
-if _name_ == "__main__":
+
+
+if __name__ == "__main__":
     # ici tu test ton code
     maze = Maze()
     maze.set_grid()
-
-
+    print(maze.grid)
